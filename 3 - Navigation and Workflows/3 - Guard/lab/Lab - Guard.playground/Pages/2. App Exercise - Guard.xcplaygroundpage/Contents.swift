a0d56063278cfe44550eb5ef91edca21f6e2a0f7
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-01-22 23:35:00 +0200

    finish guard

--- 3 - Navigation and Workflows/3 - Guard/lab/Lab - Guard.playground/Pages/2. App Exercise - Guard.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/3 - Guard/lab/Lab - Guard.playground/Pages/2. App Exercise - Guard.xcplaygroundpage/Contents.swift
@@ -10,7 +10,18 @@ import UIKit
  
  Write a failable initializer that takes parameters for your start and end times, and then checks to see if they are greater than 10 seconds apart using a guard statement. If they are, your initializer should fail. Otherwise, the initializer should set the properties accordingly.
  */
-
+struct Workout {
+    var startTime: Double
+    var endTime: Double
+    init?(startTime: Double, endTime: Double) {
+        guard endTime - startTime > 10 else {
+            return nil
+        }
+        self.endTime = endTime
+        self.startTime = startTime
+        
+    }
+}
 
 /*:
  Imagine a screen where a user inputs a meal that they've eaten. If the user taps a "save" button without adding any food, you might want to prompt the user that they haven't actually added anything.
@@ -26,13 +37,34 @@ let foodTextField = UITextField()
 let caloriesTextField = UITextField()
 
 foodTextField.text = "Banana"
-caloriesTextField.text = "23"
+caloriesTextField.text = "23w"
+
+func logFood() -> Food? {
+    guard let food = foodTextField.text,
+        let calories = caloriesTextField.text else {
+        return nil
+    }
+    guard let cals = Int(calories) else {
+        return nil
+    }
+    return Food(name: food, calories: cals)
+    
+}
 
 
 /*:
  Call the function you made above and capture the return value. Unwrap the `Food` object with standard optional binding and print a statement about the food using each of its properties. Go back and change the text in `caloriesTextField` to a string that cannot be converted into a number. What happens in that case?
  */
+let someFood = logFood()
 
+if let theFood = someFood {
+    print(theFood.name)
+    print(theFood.calories)
+    
+}
+
+
+print(someFood)
 
 /*:
  
