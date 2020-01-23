de27fbadb945d2822a2082ac4fd64617029061ba
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-01-23 23:32:16 +0200

    finish enum

---
+++ 3 - Navigation and Workflows/TrafficSegues/TrafficSegues/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Volodymyr Ostapyshyn on 23.01.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+}
+
