eae47060c4ba15ef81d5a2f87bddde575ed1fea0
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-12-25 21:23:46 +0200

    Finish step 3

--- 4 - Tables and Persistence/AppEventCount2020/AppEventCount2020/ViewController.swift
+++ 4 - Tables and Persistence/AppEventCount2020/AppEventCount2020/ViewController.swift
@@ -12,16 +12,34 @@ class ViewController: UIViewController {
     @IBOutlet var didFinishLaunching: UILabel!
     @IBOutlet var configurationForConnecting: UILabel!
     @IBOutlet var willConnectTo: UILabel!
-    @IBOutlet var DidBecomeActive: UILabel!
-    @IBOutlet var WillResignActive: UILabel!
-    @IBOutlet var WillEnterForeground: UILabel!
-    @IBOutlet var DidEnterBackground: UILabel!
-
+    @IBOutlet var didBecomeActive: UILabel!
+    @IBOutlet var willResignActive: UILabel!
+    @IBOutlet var willEnterForeground: UILabel!
+    @IBOutlet var didEnterBackground: UILabel!
+    
+    var willConnectCount = 0
+    var didBecomeActiveCount = 0
+    var willResignActiveCount = 0
+    var willEnterForegroundCount = 0
+    var didEnterBackgroundCount = 0
+    
+    var appDelegate = (UIApplication.shared.delegate as! AppDelegate)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func updateView() {
+        didFinishLaunching.text = "The App has launched \(appDelegate.launchCount) time(s)"
+        configurationForConnecting.text = "configurationForConnecting \(appDelegate.configurationForConnectingCount) time(s)"
+        willConnectTo.text = "willConnectTo \(willConnectCount) time(s)"
+        didBecomeActive.text = "didBecomeActive \(didBecomeActiveCount) time(s)"
+        willResignActive.text = "willResignActive \(willResignActiveCount) time(s)"
+        willEnterForeground.text = "willEnterForeground \(willEnterForegroundCount) time(s)"
+        didEnterBackground.text = "didEnterBackground \(didEnterBackgroundCount) time(s)"
+    }
+    
+    
 }
 
