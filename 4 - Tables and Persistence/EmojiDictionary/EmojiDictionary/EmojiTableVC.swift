5ea837c20a318ec291a622ed27b50bb6dd38ba54
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-01-02 16:16:39 +0200

    Finish Lab Remember your Emojis

--- 4 - Tables and Persistence/EmojiDictionary/EmojiDictionary/EmojiTableVC.swift
+++ 4 - Tables and Persistence/EmojiDictionary/EmojiDictionary/EmojiTableVC.swift
@@ -10,60 +10,11 @@ import UIKit
 
 class EmojiTableVC: UITableViewController {
     
-    var emojis: [Emoji] = [
-        Emoji(symbol: "😀",
-              name: "Grinning Face",
-              description: "A typical smiley face.",
-              usage: "happiness"),
-        Emoji(symbol: "😕",
-              name: "Confused Face",
-              description: "A confused, puzzled face.",
-              usage: "unsure what to think; displeasure"),
-        Emoji(symbol: "😍",
-              name: "Heart Eyes",
-              description: "A smiley face with hearts for eyes.",
-              usage: "love of something; attractive"),
-        Emoji(symbol: "👮",
-              name: "Police Officer",
-              description: "A police officer wearing a blue cap with a gold badge.",
-              usage: "person of authority"),
-        Emoji(symbol: "🐢",
-              name: "Turtle",
-              description: "A cute turtle.",
-              usage: "Something slow"),
-        Emoji(symbol: "🐘",
-              name: "Elephant",
-              description: "A gray elephant.",
-              usage: "good memory"),
-        Emoji(symbol: "🍝",
-              name: "Spaghetti",
-              description: "A plate of spaghetti.",
-              usage: "spaghetti"),
-        Emoji(symbol: "🎲",
-              name: "Die",
-              description: "A single die.",
-              usage: "taking a risk, chance; game"),
-        Emoji(symbol: "⛺️",
-              name: "Tent",
-              description: "A small tent.",
-              usage: "camping"),
-        Emoji(symbol: "📚",
-              name: "Stack of Books",
-              description: "Three colored books stacked on each other.",
-              usage: "homework, studying"),
-        Emoji(symbol: "💔",
-              name: "Broken Heart",
-              description: "A red, broken heart.",
-              usage: "extreme sadness"),
-        Emoji(symbol: "💤",
-              name: "Snore",
-              description: "Three blue \'z\'s.",
-              usage: "tired, sleepiness"),
-        Emoji(symbol: "🏁",
-              name: "Checkered Flag",
-              description: "A black-and-white checkered flag.",
-              usage: "completion")
-    ]
+    var emojis: [Emoji] = [] {
+        willSet {
+            Emoji.saveToFile(emojis: newValue)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,6 +23,13 @@ class EmojiTableVC: UITableViewController {
         
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 44
+        
+        let emojis = Emoji.loadFromFile()
+        if !emojis.isEmpty {
+            self.emojis = emojis
+        } else {
+            self.emojis = Emoji.sampleEmojis
+        }
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
