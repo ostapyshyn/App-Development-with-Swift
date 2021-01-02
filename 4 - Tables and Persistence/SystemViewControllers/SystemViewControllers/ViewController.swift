b1173e44324dd38d298156133651fbaab963165b
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-01-02 17:06:31 +0200

    Add SystemViewControllers Project

---
+++ 4 - Tables and Persistence/SystemViewControllers/SystemViewControllers/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Volodymyr Ostapyshyn on 02.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+
+
+}
+
