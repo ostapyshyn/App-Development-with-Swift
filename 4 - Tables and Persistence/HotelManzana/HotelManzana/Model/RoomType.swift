235afd0d423f9d3f39a782292fc091bd80510096
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-01-06 14:08:33 +0200

    Add delegate

--- 4 - Tables and Persistence/HotelManzana/HotelManzana/Model/RoomType.swift
+++ 4 - Tables and Persistence/HotelManzana/HotelManzana/Model/RoomType.swift
@@ -13,6 +13,12 @@ struct RoomType: Equatable {
     var shortName: String
     var price: Int
     
+    static var all: [RoomType] {
+        return [RoomType(id: 0, name: "Two Queens", shortName: "2Q", price: 179),
+                RoomType(id: 1, name: "One King", shortName: "K", price: 209),
+                RoomType(id: 2, name: "Penthouse Suite", shortName: "PHS", price: 309)]
+    }
+    
     //Equatable Protocol Implementation for RoomType
     static func ==(lhs: RoomType, rhs: RoomType) -> Bool {
         return lhs.id == rhs.id
