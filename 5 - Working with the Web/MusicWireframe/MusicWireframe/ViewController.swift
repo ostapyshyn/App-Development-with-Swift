fa71eceed532066b315abb590ca2de4146a99e49
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-01-15 19:04:46 +0200

    Add MusicFrame

---
+++ 5 - Working with the Web/MusicWireframe/MusicWireframe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicWireframe
+//
+//  Created by Volodymyr Ostapyshyn on 15.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
