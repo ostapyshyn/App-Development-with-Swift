cad9c4d8bc717633ba280bf6cc2c60a1d51e08cd
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-01-17 11:37:50 +0200

    Finish Contest

--- 5 - Working with the Web/Contest/Contest/ViewController.swift
+++ 5 - Working with the Web/Contest/Contest/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitButton(_ sender: UIButton) {
+        if textField.text?.isEmpty == true {
+            UIView.animate(withDuration: 0.5, delay: 0.0,
+                           usingSpringWithDamping: 0.5, initialSpringVelocity: 0.3,
+                           options: [], animations: {
+                            self.textField.transform = CGAffineTransform(translationX: 12, y: 0)
+                           }, completion: {_ in
+                            self.textField.transform = CGAffineTransform.identity
+                           })
+            
+        } else {
+            performSegue(withIdentifier: "second", sender: self)
+        }
+        
+    }
+    
 }
 
