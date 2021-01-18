3c7e93bc250c6e3e75c337f309eb0529cbc3287b
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-01-18 18:47:57 +0200

    Add Model

--- 5 - Working with the Web/Working with the web.playground/Contents.swift
+++ 5 - Working with the Web/Working with the web.playground/Contents.swift
@@ -1,14 +1,38 @@
 import UIKit
 import PlaygroundSupport
+
+struct PhotoInfo: Codable {
+    var title: String
+    var description: String
+    var url: URL
+    var copyright: String?
+
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description = "explanation"
+        case url
+        case copyright
+    }
+}
+
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+var urlComponents = URLComponents(string:
+   "https://api.nasa.gov/planetary/apod")!
+urlComponents.queryItems = [
+    "api_key": "DEMO_KEY",
+    "date": "2013-07-16"
+].map { URLQueryItem(name: $0.key, value: $0.value) }
+
 let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")!
-let task = URLSession.shared.dataTask(with: url) { (data,
-   response, error) in
+let task = URLSession.shared.dataTask(with: urlComponents.url!) { (data, response, error) in
+    let jsonDecoder = JSONDecoder()
     if let data = data,
-        let string = String(data: data, encoding: .utf8) {
-        print(string)
+        let photoInfo = try? jsonDecoder.decode(PhotoInfo.self, from: data) {
+        print(photoInfo)
     }
     PlaygroundPage.current.finishExecution()
 }
+
 task.resume()
+
