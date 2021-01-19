71ae186d0abe301882398330918ab27a0e0b7ff8
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-01-19 11:42:54 +0200

    Add ResultType

--- 5 - Working with the Web/Working with the web.playground/Contents.swift
+++ 5 - Working with the Web/Working with the web.playground/Contents.swift
@@ -17,22 +17,38 @@ struct PhotoInfo: Codable {
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-var urlComponents = URLComponents(string:
-   "https://api.nasa.gov/planetary/apod")!
-urlComponents.queryItems = [
-    "api_key": "DEMO_KEY",
-    "date": "2013-07-16"
-].map { URLQueryItem(name: $0.key, value: $0.value) }
+func fetchPhotoInfo(completion: @escaping (Result<PhotoInfo, Error>) -> Void) {
+    var urlComponents = URLComponents(string: "https://api.nasa.gov/planetary/apod")!
+    urlComponents.queryItems = [
+        "api_key": "DEMO_KEY",
+    ].map { URLQueryItem(name: $0.key, value: $0.value) }
 
-let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")!
-let task = URLSession.shared.dataTask(with: urlComponents.url!) { (data, response, error) in
-    let jsonDecoder = JSONDecoder()
-    if let data = data,
-        let photoInfo = try? jsonDecoder.decode(PhotoInfo.self, from: data) {
-        print(photoInfo)
+    let task = URLSession.shared.dataTask(with: urlComponents.url!) { (data, response, error) in
+        let jsonDecoder = JSONDecoder()
+        if let data = data {
+            do {
+                let photoInfo = try jsonDecoder.decode(PhotoInfo.self, from: data)
+                completion(.success(photoInfo))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
     }
+
+    task.resume()
+}
+
+fetchPhotoInfo { (result) in
+    switch result {
+    case .success(let photoInfo):
+        print("Successfully fetched PhotoInfo: \(photoInfo)")
+    case .failure(let error):
+        print("Fetch PhotoInfo Failed with Error: \(error)")
+    }
+
     PlaygroundPage.current.finishExecution()
 }
 
-task.resume()
+
 
