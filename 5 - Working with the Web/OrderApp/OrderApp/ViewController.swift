3545087d8bbb631feb250e6c99da2cecb08b1ea3
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-01-23 21:34:30 +0200

    Add Order App

---
+++ 5 - Working with the Web/OrderApp/OrderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrderApp
+//
+//  Created by Volodymyr Ostapyshyn on 23.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
