288f75c625d7fe9e2d969ae206b935790ca6bfd5
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-01-25 08:45:50 +0200

    Add updateUI

--- 5 - Working with the Web/OrderApp/OrderApp/MenuController.swift
+++ 5 - Working with the Web/OrderApp/OrderApp/MenuController.swift
@@ -11,24 +11,77 @@ class MenuController {
     typealias MinutesToPrepare = Int
     let baseURL = URL(string: "http://localhost:8080/")!
     
-    func fetchCategories(completion: @escaping (Result<[String],
-       Error>) -> Void) {
+    func fetchCategories(completion: @escaping (Result<[String], Error>) -> Void) {
         let categoriesURL = baseURL.appendingPathComponent("categories")
+        let task = URLSession.shared.dataTask(with: categoriesURL) { (data, response, error) in
+            if let data = data {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let categoriesResponse = try
+                        jsonDecoder.decode(CategoriesResponse.self, from: data)
+                    completion(.success(categoriesResponse.categories))
+                } catch {
+                    completion(.failure(error))
+                }
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
     }
     
     func fetchMenuItems(forCategory categoryName: String,
-       completion: @escaping (Result<[MenuItem], Error>) -> Void) {
+                        completion: @escaping (Result<[MenuItem], Error>) -> Void) {
         let baseMenuURL = baseURL.appendingPathComponent("menu")
-            var components = URLComponents(url: baseMenuURL,
-               resolvingAgainstBaseURL: true)!
-            components.queryItems = [URLQueryItem(name: "category",
-               value: categoryName)]
-            let menuURL = components.url!
+        var components = URLComponents(url: baseMenuURL,
+                                       resolvingAgainstBaseURL: true)!
+        components.queryItems = [URLQueryItem(name: "category",
+                                              value: categoryName)]
+        let menuURL = components.url!
+        let task = URLSession.shared.dataTask(with: menuURL) { (data, response, error) in
+            if let data = data {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let menuResponse = try
+                        jsonDecoder.decode(MenuResponse.self, from: data)
+                    completion(.success(menuResponse.items))
+                } catch {
+                    completion(.failure(error))
+                }
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
     }
     
     func submitOrder(forMenuIDs menuIDs: [MinutesToPrepare], completion:
-       @escaping (Result<Int, Error>) -> Void) {
+                        @escaping (Result<Int, Error>) -> Void) {
         let orderURL = baseURL.appendingPathComponent("order")
+        var request = URLRequest(url: orderURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let data = ["menuIds": menuIDs]
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(data)
+        request.httpBody = jsonData
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let orderResponse = try
+                        jsonDecoder.decode(OrderResponse.self, from: data)
+                    completion(.success(orderResponse.prepTime))
+                } catch {
+                    completion(.failure(error))
+                }
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
     }
+    
+    
     
 }
