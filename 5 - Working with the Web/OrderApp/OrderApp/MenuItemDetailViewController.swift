2e8aa9612dcb48214af17567dff9e04bf8beb3a1
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-01-27 18:38:30 +0200

    Add orderButtonTapped

--- 5 - Working with the Web/OrderApp/OrderApp/MenuItemDetailViewController.swift
+++ 5 - Working with the Web/OrderApp/OrderApp/MenuItemDetailViewController.swift
@@ -22,6 +22,13 @@ class MenuItemDetailViewController: UIViewController {
         super.init(coder: coder)
     }
     
+    func updateUI() {
+        nameLabel.text = menuItem.name
+        priceLabel.text = MenuItem.priceFormatter.string(from:
+           NSNumber(value: menuItem.price))
+        detailTextLabel.text = menuItem.detailText
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -29,9 +36,22 @@ class MenuItemDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addToOrderButton.layer.cornerRadius = 5.0
+        updateUI()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func orderButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0,
+           usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1,
+           options: [], animations: {
+            self.addToOrderButton.transform =
+               CGAffineTransform(scaleX: 2.0, y: 2.0)
+            self.addToOrderButton.transform =
+               CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }, completion: nil)
+    }
+    
     
 
     /*
