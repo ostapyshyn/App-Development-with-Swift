73faef047ea2e0b4b6a23871e62199de7695233e
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-02-01 08:31:59 +0200

    Add StateRestorationController

--- 5 - Working with the Web/OrderApp/OrderApp/Restoration.swift
+++ 5 - Working with the Web/OrderApp/OrderApp/Restoration.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
+enum StateRestorationController {
+    
+    enum Identifier: String {
+        case categories, menu, menuItemDetail, order
+    }
+    
+    case categories
+    case menu(category: String)
+    case menuItemDetail(MenuItem)
+    case order
+    
+    var identifier: Identifier {
+        switch self {
+        case .categories: return Identifier.categories
+        case .menu: return Identifier.menu
+        case .menuItemDetail: return Identifier.menuItemDetail
+        case .order: return Identifier.order
+        }
+    }
+}
+
 extension NSUserActivity {
 
     var order: Order? {
@@ -27,5 +48,45 @@ extension NSUserActivity {
             }
         }
     }
+    
+    var controllerIdentifier: StateRestorationController.Identifier? {
+        get {
+            if let controllerIdentifierString = userInfo?["controllerIdentifier"] as? String {
+                return StateRestorationController.Identifier(rawValue: controllerIdentifierString)
+            } else {
+                return nil
+            }
+        }
+        set {
+            userInfo?["controllerIdentifier"] = newValue?.rawValue
+        }
+    }
+    
+    var menuCategory: String? {
+        get {
+            return userInfo?["menuCategory"] as? String
+        }
+        set {
+            userInfo?["menuCategory"] = newValue
+        }
+    }
+    
+    var menuItem: MenuItem? {
+        get {
+            guard let jsonData = userInfo?["menuItem"] as? Data else {
+                return nil
+            }
+            return try? JSONDecoder().decode(MenuItem.self, from: jsonData)
+        }
+        set {
+            if let newValue = newValue, let jsonData = try? JSONEncoder().encode(newValue) {
+                addUserInfoEntries(from: ["menuItem": jsonData])
+            } else {
+                userInfo?["menuItem"] = nil
+            }
+        }
+    }
+
+    
 }
 
