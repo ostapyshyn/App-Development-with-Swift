4918e25e8c04b4b65ed2ac63c4e29ab6aadb125a
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2021-02-03 22:38:33 +0200

    Add BasicCollectionView

---
+++ AdvancedDataDisplay/BasicCollectionView/BasicCollectionView/BasicCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BasicCollectionViewCell.swift
+//  BasicCollectionView
+//
+//  Created by Volodymyr Ostapyshyn on 02.02.2021.
+//
+
+import UIKit
+
+class BasicCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var label: UILabel!
+    
+    
+}
---
+++ AdvancedDataDisplay/BasicCollectionView/BasicCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicCollectionView
+//
+//  Created by Volodymyr Ostapyshyn on 02.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
